6f7ac6adc02b2ebdc14e92cd3872f705cc23d994
pola-raouf <pola2302440@miuegypt.edu.eg>
2025-12-05 01:34:45 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -79,13 +79,13 @@ Route::middleware(['auth'])->group(function () {
 Route::get('/properties', [PropertyController::class, 'index'])->name('properties.index');
 Route::get('/properties/create', [PropertyController::class, 'create'])->name('properties.create');
 Route::post('/properties', [PropertyController::class, 'store'])->name('properties.store');
-
+Route::middleware(['auth'])->group(function () {
 Route::get('/properties/{property}', [PropertyController::class, 'show'])->name('properties.show');
 
 Route::get('/properties/{property}/edit', [PropertyController::class, 'edit'])->name('properties.edit');
 Route::put('/properties/{property}', [PropertyController::class, 'update'])->name('properties.update');
 Route::delete('/properties/{property}', [PropertyController::class, 'destroy'])->name('properties.destroy');
-
+});
 // Reservations
 // web.php
 Route::middleware('auth')->group(function () {
