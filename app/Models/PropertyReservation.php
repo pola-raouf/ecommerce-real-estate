514cd99a1443a7c5f1d5884ea3b4d03b49908f6d
eyad-404 <eyadops.ws@gmail.com>
2025-12-05 00:09:07 +0200

    Update PropertyReservation.php

--- app/Models/PropertyReservation.php
+++ app/Models/PropertyReservation.php
@@ -7,14 +7,4 @@ use Illuminate\Database\Eloquent\Model;
 class PropertyReservation extends Model
 {
     protected $fillable = ['property_id', 'user_id', 'reserved_at'];
-
-    public function property()
-    {
-        return $this->belongsTo(Property::class);
-    }
-
-    public function user()
-    {
-        return $this->belongsTo(User::class);
-    }
 }
