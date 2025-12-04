8e151af43afac7aed44a7479a9b6f71055276f9b
eyad-404 <eyadops.ws@gmail.com>
2025-12-05 00:31:03 +0200

    Update index.blade.php

--- resources/views/properties/index.blade.php
+++ resources/views/properties/index.blade.php
@@ -11,7 +11,7 @@
 </head>
 <body>
 
-<!-- ================= NAVBAR ================= -->
+<!-- ===========-====== NAVBAR ================= -->
 <nav id="mainNavbar" class="navbar navbar-expand-lg navbar-dark fixed-top">
     <div class="container">
         <a class="navbar-brand fw-bold fs-4" href="{{ url('/') }}">
