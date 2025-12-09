65315c789f57e4e925966a020cfa22c7cf0e71e7
eyad-404 <eyadops.ws@gmail.com>
2025-12-09 22:31:46 +0200

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -42,7 +42,7 @@ route::middleware('guest')->group(function(){
 Route::get('/register', [AuthController::class, 'showRegister'])->name('register.form');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
 
-Route::get('/login', [AuthController::class, 'showlogin'])->name('login.form');
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login.form');
 Route::post('/login', [AuthController::class, 'login'])->name('login');
 });
 
