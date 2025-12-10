349eff76df616b85e255491d84cf35c044774253
eyad-404 <eyadops.ws@gmail.com>
2025-12-10 08:29:37 +0200

    Update property-management.blade.php

--- resources/views/properties/property-management.blade.php
+++ resources/views/properties/property-management.blade.php
@@ -201,6 +201,7 @@
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/js/bootstrap.bundle.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
 <script src="{{ asset('js/property-management.js') }}"></script>
+<script src="{{ asset('js/button-loader.js') }}"></script>
 <script src="{{ asset('js/property-management/edit-modal.js') }}"></script>
 <script src="{{ asset('js/property-management/delete-modal.js') }}"></script>
 
